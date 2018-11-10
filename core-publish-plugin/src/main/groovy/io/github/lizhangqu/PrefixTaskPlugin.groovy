35be5c3b62b6ec8d117f663888164b23bda66331
李樟取 <lizhangqu@weidian.com>
2018-11-10 18:21:50 +0800

    * prefix

--- core-publish-plugin/src/main/groovy/io/github/lizhangqu/PrefixTaskPlugin.groovy
+++ core-publish-plugin/src/main/groovy/io/github/lizhangqu/PrefixTaskPlugin.groovy
@@ -1,18 +1,15 @@
 package io.github.lizhangqu
 
-import org.gradle.api.*
+import org.gradle.api.Project
 
 /**
  * 添加前缀task，默认不添加，如果配置了ENABLE_PREFIX_TASK=true，则添加
  */
-class PrefixTaskPlugin implements Plugin<Project> {
-    private Properties properties = new Properties()
+class PrefixTaskPlugin extends BasePropertiesPlugin {
 
     @Override
     void apply(Project project) {
-        //read local properties
-        loadLocalProperties(project)
-
+        super.apply(project)
         def enablePrefixTask = getEnablePrefixTask(project)
         if (enablePrefixTask != null && String.valueOf(enablePrefixTask).equalsIgnoreCase("true")) {
             project.afterEvaluate {
@@ -36,43 +33,20 @@ class PrefixTaskPlugin implements Plugin<Project> {
         }
     }
 
-    def loadLocalProperties(Project project) {
-        try {
-            File localFile = project.rootProject.file('local.properties')
-            if (localFile.exists()) {
-                properties.load(localFile.newDataInputStream())
-            }
-        } catch (Exception e) {
-            println("load local properties failed msg:${e.message}")
-        }
+    private String getEnablePrefixTask(Project project) {
+        return readPropertyFromProject(project, "ENABLE_PREFIX_TASK", 'false', false)
     }
 
-    def readPropertyFromLocalProperties(Project project, String key, String defaultValue) {
-        readPropertyFromLocalPropertiesOrThrow(project, key, defaultValue, true)
+    private String getPomGroupId(Project project) {
+        return readPropertyFromProject(project, "PROJECT_POM_GROUP_ID", project.group.toString())
     }
 
-    @SuppressWarnings("GroovyUnusedDeclaration")
-    def readPropertyFromLocalPropertiesOrThrow(Project project, String key, String defaultValue, boolean throwIfNull) {
-        def property = (properties != null && properties.containsKey(key)) ? properties.getProperty(key, defaultValue) : defaultValue
-        if (property == null && throwIfNull) {
-            throw new GradleException("you must config ${key} in properties. Like config project.ext.${key} , add ${key} in gradle.properties or add ${key} in local.properties which locates on root project dir")
-        }
-        return property
+    private String getPomArtifactId(Project project) {
+        return readPropertyFromProject(project, "PROJECT_POM_ARTIFACT_ID", getDefaultPomArtifactId(project))
     }
 
-    def getEnablePrefixTask(Project project) {
-        return project.hasProperty('ENABLE_PREFIX_TASK') ? project.ext.ENABLE_PREFIX_TASK : readPropertyFromLocalPropertiesOrThrow(project, 'ENABLE_PREFIX_TASK', 'true', false)
-    }
-
-    def getPomGroupId(Project project) {
-        return project.hasProperty('PROJECT_POM_GROUP_ID') ? project.ext.PROJECT_POM_GROUP_ID : readPropertyFromLocalProperties(project, 'PROJECT_POM_GROUP_ID', project.group.toString())
-    }
-
-    def getPomArtifactId(Project project) {
-        return project.hasProperty('PROJECT_POM_ARTIFACT_ID') ? project.ext.PROJECT_POM_ARTIFACT_ID : readPropertyFromLocalProperties(project, 'PROJECT_POM_ARTIFACT_ID', getDefaultPomArtifactId(project))
-    }
-
-    def getDefaultPomArtifactId(Project project) {
+    @SuppressWarnings("GrMethodMayBeStatic")
+    private String getDefaultPomArtifactId(Project project) {
         if (project.hasProperty('archivesBaseName')) {
             return project.archivesBaseName.toString()
         }
